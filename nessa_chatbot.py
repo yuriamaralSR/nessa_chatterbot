071ad55389b9b612bf73b8442d0c80305557d823
Yuri Amaral Santos Rocha <yuriamaral.yas@gmail.com>
2023-05-15 15:31:39 -0300

    atualização nessa_chatbot.py
    criando uma função propria para comparar a mensagem do usuario com as ja cadastradas

--- nessa_chatbot.py
+++ nessa_chatbot.py
@@ -1,11 +1,28 @@
+from difflib import SequenceMatcher
 from chatterbot import ChatBot
 from unidecode import unidecode
 
 CONFIANCA_MINIMA = 0.70
 
+def comparar_mensagens(mensagem_digitada, mensagem_candidata):
+    confianca = 0.0
+
+    digitada = mensagem_digitada.text
+    candidata = mensagem_candidata.text
+    if digitada and candidata:
+        confianca = SequenceMatcher(
+            None,
+            digitada,
+            candidata
+        )
+        confianca = round(confianca.ratio(), 2)
+
+    return confianca
+
 def iniciar():
     robo = ChatBot("Nessa Chatbot",
         read_only = True,
+        statement_comparison_function=comparar_mensagens,
         logic_adapters = [
             {
                 "import_path": "chatterbot.logic.BestMatch"
